c32cb35098a71e0462e6b1ce3e9b02067a55c93b
guanguanchuangyu <3246907890@qq.com>
2022-04-15 01:03:10 +0800

    [功能]-添加设备查找页面

---
+++ src/G2CyHome.Wpf/Views/DeviceSearch.xaml.cs
@@ -0,0 +1,33 @@
+﻿using HandyControl.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace G2CyHome.Wpf.Views
+{
+    /// <summary>
+    /// DeviceSearch.xaml 的交互逻辑
+    /// </summary>
+    public partial class DeviceSearch
+    {
+        public DeviceSearch()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_addbind_Click(object sender, RoutedEventArgs e)
+        {
+            Dialog.Show(new DeviceBind());
+        }
+    }
+}
